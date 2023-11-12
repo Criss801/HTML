11f810e5ec1ee62e52ef5a43c7519887623aca69
Criss801 <141930782+Criss801@users.noreply.github.com>
2023-11-12 18:18:24 -0500

    Add files via upload

--- Semana 16/js/Formulario Input.js
+++ Semana 16/js/Formulario Input.js
@@ -1,21 +1,21 @@
 function calcular(){
     let n1, n2, n3, definitiva, estado;
-    n1=parseFloat(document.getElementById('not1').value);
-    n2=parseFloat(document.getElementById('not2').value);
-    n2=parseFloat(document.getElementById('not3').value);
+    n1=parseFloat(document.getElementById('n1').value);
+    n2=parseFloat(document.getElementById('n2').value);
+    n3=parseFloat(document.getElementById('n3').value);
     definitiva=(n1+n2+n3)/3;
-    document.getElementById('cl').value=definitiva;
-    if (definitiva>=1 && definitiva<=2.5){
+    document.getElementById('c').value=definitiva;
+    if (definitiva>=1.0 && definitiva<=2.5){
         estado="REPROBADO";
     } else if (definitiva>=2.6 && definitiva<=3.4){
         estado= "Habilitar";
-    } else if (definitiva>=3.5 && definitiva<=4){
+    } else if (definitiva>=3.5 && definitiva<=4.0){
         estado="Aceptable";
     } else if (definitiva>=4.1){
         estado="Excelente";
     } else {
         estado="VALOR ERRONEO";
     }
-    document.getElementById('cl').value=estado;
-    document.getElementById('def').value=definitiva;
+    document.getElementById('c').value=estado;
+    document.getElementById('d').value=definitiva;
 }
